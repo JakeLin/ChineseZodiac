f543608782c161b7dcb442ee3c5ca1f9ae934b58
Jake Lin <jakelinau@gmail.com>
2014-09-22 21:11:55 +1000

    Update the code for Xcode 6 GM

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
         yearOfBirth.resignFirstResponder()
     }
 
